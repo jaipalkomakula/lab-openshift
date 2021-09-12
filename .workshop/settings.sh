833f4f8b03fe4f591269464f1511f97f0c3fabe6
jaipalkomakula <jaipalkomakula@gmail.com>
2021-09-12 12:20:53 -0600

    .workshop

---
+++ .workshop/settings.sh
@@ -0,0 +1,19 @@
+WORKSHOP_NAME=lab-openshift-workshop
+WORKSHOP_IMAGE=quay.io/kskels/lab-openshift-workshop:1.0
+WORKSHOP_TITLE="OpenShift Workshop"
+WORKSHOP_DESCRIPTION="Supporting labs for a generic OpenShift workshop"
+
+TERMINAL_IMAGE=quay.io/openshifthomeroom/workshop-terminal:3.4.3
+DASHBOARD_IMAGE=quay.io/openshifthomeroom/workshop-dashboard:5.0.1
+SPAWNER_IMAGE=quay.io/openshifthomeroom/workshop-spawner:7.2.2
+CONSOLE_IMAGE=quay.io/openshift/origin-console:4.8.0
+
+SPAWNER_VERSION=7.2.2
+#SPAWNER_MODE=learning-portal
+SPAWNER_MODE=hosted-workshop
+
+DASHBOARD_VERSION=5.0.1
+
+RESOURCE_BUDGET=medium
+MAX_SESSION_AGE=14400
+IDLE_TIMEOUT=3600
